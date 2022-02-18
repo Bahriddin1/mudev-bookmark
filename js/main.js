a76c31392cc39f5905a6c7bd21d3e11a9e7f3909
Bahriddin1 <bahrid1777@outlook.com>
2022-02-18 20:49:05 +0900

    dsdds

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
-const elSiteFeaturesSlide = document.querySelector('.slide-item-wrapper');
-const elFeaturesBookmark = document.querySelector('.features-bookmark')
-const elSiteFeaturesSlideItem = document.querySelector('.features-slide-item');
+const elSiteFeaturesSlide = document.querySelectorAll('.slide-item-wrapper');
+const elFeaturesBookmark = document.querySelectorAll('.features-bookmark')
+const elSiteFeaturesSlideItem = document.querySelectorAll('.features-slide-item');
 const elBodyScrollHidden = document.querySelector('body');
 
 
-elSiteFeaturesSlideItem.addEventListener('click', function () {
+for
+
+elSiteFeaturesSlideItem.addEventListener('click', function ()   {
   elSiteFeaturesSlideItem.classList.toggle('features-slide-item-active');
   elSiteFeaturesSlide.classList.toggle('slide-item-wrapper-active');
   elFeaturesBookmark.classList.remove('visually-hidden');
